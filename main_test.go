5c61438121cd2367f564ec394fa9ad744999e62b
Burak Bolat <burak.bolat@hprotravel.com>
2022-05-13 00:22:32 +0300

    Add another test for it doe not contains integer

--- main_test.go
+++ main_test.go
@@ -4,11 +4,19 @@ import (
 	"testing"
 )
 
-func TestContains(t *testing.T) {	
+func TestContains(t *testing.T) {
 	t.Run("test it contains integer item", func(t *testing.T) {
-		got := Contains([]int{1,2,3}, 1)
+		got := Contains([]int{1, 2, 3}, 1)
 		want := true
-	
+
+		if got != want {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+	t.Run("test it not contains integer item", func(t *testing.T) {
+		got := Contains([]int{1, 2, 3}, 9)
+		want := false
+
 		if got != want {
 			t.Errorf("got %v want %v", got, want)
 		}
