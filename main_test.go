1377aab02984278ca2860db93015fcbda4539b12
Burak Bolat <burak.bolat@hprotravel.com>
2022-05-13 00:20:22 +0300

    Refactor test func for the multi cases

--- main_test.go
+++ main_test.go
@@ -5,10 +5,12 @@ import (
 )
 
 func TestContains(t *testing.T) {	
-	got := Contains([]int{1,2,3}, 1)
-	want := true
-
-	if got != want {
-		t.Errorf("got %v want %v", got, want)
-	}
+	t.Run("test it contains integer item", func(t *testing.T) {
+		got := Contains([]int{1,2,3}, 1)
+		want := true
+	
+		if got != want {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
 }
